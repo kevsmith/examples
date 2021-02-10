7f2613cdf7b2c0cb39047e2966da91bc440d5de4
Kevin A. Smith <kevin.smith@pureport.com>
2021-02-10 11:06:29 -0500

    Completely data driven testing example

--- test/example1_data_test.exs
+++ test/example1_data_test.exs
@@ -6,13 +6,11 @@ defmodule Example1DataTest do
   # Ensure we recompile whenever test data changes
   @external_resource "test/test_data.json"
 
-  setup do
-    [data] = Jason.decode!(File.read!("test/test_data.json"))
-    %{data: data}
-  end
+  [data] = Jason.decode!(File.read!("test/test_data.json"))
 
-  test "integers", %{data: data} do
-    %{"values" => [a, b], "expected" => expected} = Map.get(data, "integers")
-    assert ^expected = Example1.add(a, b)
+  for {name, %{"expected" => expected, "values" => [a, b]}} <- data do
+    test "#{name}" do
+      assert unquote(expected) = Example1.add(unquote(a), unquote(b))
+    end
   end
 end
