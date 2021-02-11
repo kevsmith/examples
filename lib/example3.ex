5cc6f58d5327d6934e58877e2e09d64b9b8c0586
Kevin A. Smith <kevin.smith@pureport.com>
2021-02-11 15:35:36 -0500

    Added example3 code

---
+++ lib/example3.ex
@@ -0,0 +1,20 @@
+defmodule Examples.Example3 do
+  def count_em(data), do: count_em(data, %{})
+
+  def count_em([], counts) do
+    counts
+  end
+
+  def count_em([item | t], counts) do
+    count_em(t, Map.update(counts, item, 1, &(&1 + 1)))
+  end
+
+  def reduce_em(data) do
+    Enum.reduce(data, %{}, &Map.update(&2, &1, 1, fn x -> x + 1 end))
+  end
+
+  def group_em(data) do
+    Enum.group_by(data, & &1, & &1)
+    |> Enum.reduce(%{}, fn {key, values}, acc -> Map.put(acc, key, length(values)) end)
+  end
+end
